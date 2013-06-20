76fcc287eb77ba9648242d5201f45011c3dc1882
Brian Lewis <brian@lorf.org>
2013-06-20 00:11:09 -0500

    misc

---
+++ src/Gear.hs
@@ -0,0 +1,122 @@
+module Gear
+  ( makeGear
+  ) where
+
+import Control.Monad (forM_, when)
+import qualified Graphics.Rendering.OpenGL as GL
+
+makeGear :: Float -> Float -> Float -> Int -> Float -> GL.Color4 GL.GLfloat -> IO GL.DisplayList
+makeGear inradius outradius width teeth toothdepth color =
+    GL.defineNewList GL.Compile $ do
+        GL.colorMaterial GL.$= Just (GL.Front, GL.AmbientAndDiffuse)
+        GL.color color
+        drawGear
+  where
+    drawGear = do
+        let r0   = inradius
+            r1   = outradius - toothdepth / 2
+            r2   = outradius + toothdepth / 2
+            pi2  = 2 * pi
+            da   = pi2 / realToFrac teeth / 4 :: Float
+            wd2  = width * 0.5
+            wd2n = negate wd2
+
+            calca :: Int -> Float
+            calca i = realToFrac i * pi2 / realToFrac teeth :: Float
+
+        GL.shadeModel GL.$= GL.Flat
+        GL.normal (GL.Normal3 0 0 1 :: GL.Normal3 GL.GLfloat)
+
+        -- front face
+        GL.renderPrimitive GL.QuadStrip $
+            forM_ [0 .. teeth] $ \i -> do
+                let a = calca i
+                vertex (r0 * cos a) (r0 * sin a) wd2
+                vertex (r1 * cos a) (r1 * sin a) wd2
+                when (i < teeth) $ do
+                    vertex (r0 * cos a) (r0 * sin a) wd2
+                    vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2
+
+        -- front sides of teeth
+        GL.renderPrimitive GL.Quads $
+            forM_ [0 .. pred teeth] $ \i -> do
+                let a = calca i
+                vertex (r1 * cos a) (r1 * sin a) wd2
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2
+
+        GL.normal (GL.Normal3 0 0 (-1) :: GL.Normal3 GL.GLfloat)
+
+        -- back face
+        GL.renderPrimitive GL.QuadStrip $
+            forM_ [0 .. teeth] $ \i -> do
+                let a = calca i
+                vertex (r1 * cos a) (r1 * sin a) wd2n
+                vertex (r0 * cos a) (r0 * sin a) wd2n
+                when (i < teeth) $ do
+                    vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2n
+                    vertex (r0 * cos a) (r0 * sin a) wd2n
+
+        -- back sides of teeth
+        GL.renderPrimitive GL.Quads $
+            forM_ [0 .. pred teeth] $ \i -> do
+                let a = calca i
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2n
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2n
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2n
+                vertex (r1 * cos a) (r1 * sin a) wd2n
+
+        -- outward faces of teeth
+        GL.renderPrimitive GL.QuadStrip $ do
+            forM_ [0 .. pred teeth] $ \i -> do
+                let a = calca i
+                vertex (r1 * cos a) (r1 * sin a) wd2
+                vertex (r1 * cos a) (r1 * sin a) wd2n
+                let u = r2 * cos (a + da) - r1 * cos a
+                    v = r2 * sin (a + da) - r1 * sin a
+                    len = sqrt (u * u + v * v)
+                    un = u / len
+                    vn = v / len
+                normal vn (-un) 0
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2n
+                normal (cos a) (sin a) 0
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2n
+                let u' = r1 * cos (a + 3 * da) - r2 * cos (a + 2 * da)
+                    v' = r1 * sin (a + 3 * da) - r2 * sin (a + 2 * da)
+                normal v' (-u') 0
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2n
+                normal (cos a) (sin a) 0
+
+            vertex (r1 * cos 0) (r1 * sin 0) wd2
+            vertex (r1 * cos 0) (r1 * sin 0) wd2n
+
+        GL.shadeModel GL.$= GL.Smooth
+
+        -- inside radius cylinder
+        GL.renderPrimitive GL.QuadStrip $
+            forM_ [0 .. teeth] $ \i -> do
+                let a = calca i
+                normal (negate $ cos a) (negate $ sin a) 0
+                vertex (r0 * cos a) (r0 * sin a) wd2n
+                vertex (r0 * cos a) (r0 * sin a) wd2
+
+vertex :: Float -> Float -> Float -> IO ()
+vertex x y z =
+    GL.vertex $ mkVertex x y z
+
+mkVertex :: Float -> Float -> Float -> GL.Vertex3 GL.GLfloat
+mkVertex x y z =
+    GL.Vertex3 (realToFrac x) (realToFrac y) (realToFrac z)
+
+normal :: Float -> Float -> Float -> IO ()
+normal x y z =
+    GL.normal $ mkNormal x y z
+
+mkNormal :: Float -> Float -> Float -> GL.Normal3 GL.GLfloat
+mkNormal x y z =
+    GL.Normal3 (realToFrac x) (realToFrac y) (realToFrac z)
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 
-import Control.Monad            (forM_, unless, when, void)
+import Control.Monad            (unless, void)
 import Control.Monad.RWS.Strict (RWST, ask, evalRWST, get, liftIO, put)
 
 import qualified Graphics.Rendering.OpenGL as GL
 
 import qualified Graphics.UI.GLFW as GLFW
+
+import Gear (makeGear)
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -59,9 +61,9 @@ main = do
     GL.clearColor GL.$= GL.Color4 0.05 0.05 0.05 1
     GL.normalize  GL.$= GL.Enabled
 
-    gear1 <- mkGear1
-    gear2 <- mkGear2
-    gear3 <- mkGear3
+    gear1 <- makeGear 1   4 1   20 0.7 (GL.Color4 0.8 0.1 0   1)  -- red
+    gear2 <- makeGear 0.5 2 2   10 0.7 (GL.Color4 0   0.8 0.2 1)  -- green
+    gear3 <- makeGear 1.3 2 0.5 10 0.7 (GL.Color4 0.2 0.2 1   1)  -- blue
 
     let env = Env
           { envGear1  = gear1
@@ -70,10 +72,10 @@ main = do
           , envWindow = w
           }
         state = State
-          { stateViewXAngle = 45
-          , stateViewYAngle =  0
-          , stateViewZAngle =  0
-          , stateGearAngle  =  0
+          { stateViewXAngle = 0
+          , stateViewYAngle = 0
+          , stateViewZAngle = 0
+          , stateGearAngle  = 0
           }
 
     run env state
@@ -85,15 +87,15 @@ main = do
 
 errorCallback :: GLFW.Error -> String -> IO ()
 errorCallback e s =
-    printFields ["error", show e, show s]
+    printFields "error" [show e, show s]
 
 windowPosCallback :: GLFW.Window -> Int -> Int -> IO ()
 windowPosCallback _ x y =
-    printFields ["windowPos", show x, show y]
+    printFields "windowPos" [show x, show y]
 
 windowSizeCallback :: GLFW.Window -> Int -> Int -> IO ()
 windowSizeCallback _ width height = do
-    printFields ["windowSize", show width, show height]
+    printFields "windowSize" [show width, show height]
     let pos  = GL.Position 0 0
         size = GL.Size (fromIntegral width) (fromIntegral height)
         h     = fromIntegral height / fromIntegral width :: Float
@@ -104,63 +106,63 @@ windowSizeCallback _ width height = do
     GL.matrixMode GL.$= GL.Projection
     GL.loadIdentity
     GL.frustum (realToFrac $ -xmax)
-               (realToFrac xmax)
+               (realToFrac    xmax)
                (realToFrac $ -xmax * realToFrac h)
-               (realToFrac $ xmax * realToFrac h)
-               (realToFrac znear)
-               (realToFrac zfar)
+               (realToFrac $  xmax * realToFrac h)
+               (realToFrac    znear)
+               (realToFrac    zfar)
     GL.matrixMode GL.$= GL.Modelview 0
     GL.loadIdentity
     GL.translate (GL.Vector3 0 0 (-20) :: GL.Vector3 GL.GLfloat)
 
 windowCloseCallback :: GLFW.Window -> IO ()
 windowCloseCallback _ =
-    printFields ["windowClose"]
+    printFields "windowClose" []
 
 windowRefreshCallback :: GLFW.Window -> IO ()
 windowRefreshCallback _ =
-    printFields ["windowRefresh"]
+    printFields "windowRefresh" []
 
 windowFocusCallback :: GLFW.Window -> GLFW.FocusAction -> IO ()
 windowFocusCallback _ fa =
-    printFields ["windowFocus", show fa]
+    printFields "windowFocus" [show fa]
 
 windowIconifyCallback :: GLFW.Window -> GLFW.IconifyAction -> IO ()
 windowIconifyCallback _ ia =
-    printFields ["windowIconify", show ia]
+    printFields "windowIconify" [show ia]
 
 framebufferSizeCallback :: GLFW.Window -> Int -> Int -> IO ()
 framebufferSizeCallback _ width height =
-    printFields ["framebufferSize", show width, show height]
+    printFields "framebufferSize" [show width, show height]
 
 mouseButtonCallback :: GLFW.Window -> GLFW.MouseButton -> GLFW.MouseButtonAction -> GLFW.ModifierKeys -> IO ()
 mouseButtonCallback _ mb mba mk =
-    printFields ["mouseButton", show mb, show mba, show mk]
+    printFields "mouseButton" [show mb, show mba, show mk]
 
 cursorPosCallback :: GLFW.Window -> Double -> Double -> IO ()
 cursorPosCallback _ x y =
-    printFields ["cursorPos", show x, show y]
+    printFields "cursorPos" [show x, show y]
 
 cursorEnterCallback :: GLFW.Window -> GLFW.CursorAction -> IO ()
 cursorEnterCallback _ ca =
-    printFields ["cursorEnter", show ca]
+    printFields "cursorEnter" [show ca]
 
 scrollCallback :: GLFW.Window -> Double -> Double -> IO ()
 scrollCallback _ x y =
-    printFields ["scroll", show x, show y]
+    printFields "scroll" [show x, show y]
 
 keyCallback :: GLFW.Window -> GLFW.Key -> Int -> GLFW.KeyAction -> GLFW.ModifierKeys -> IO ()
 keyCallback _ k sc ka mk =
-    printFields ["key", show k, show sc, show ka, show mk]
+    printFields "key" [show k, show sc, show ka, show mk]
 
 charCallback :: GLFW.Window -> Char -> IO ()
 charCallback _ c =
-    printFields ["char", show c]
+    printFields "char" [show c]
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-printFields :: [String] -> IO ()
-printFields = putStrLn . unwords
+printFields :: String -> [String] -> IO ()
+printFields cbname fields = putStrLn $ cbname ++ ": " ++ unwords fields
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -229,130 +231,6 @@ draw = do
         xunit = GL.Vector3 1 0 0 :: GL.Vector3 GL.GLfloat
         yunit = GL.Vector3 0 1 0 :: GL.Vector3 GL.GLfloat
         zunit = GL.Vector3 0 0 1 :: GL.Vector3 GL.GLfloat
-
-mkGear1 :: IO GL.DisplayList
-mkGear1 =
-    GL.defineNewList GL.Compile $ do
-        GL.colorMaterial GL.$= Just (GL.Front, GL.AmbientAndDiffuse)
-        GL.color red
-        makeGear 1 4 1 20 0.7
-      where
-        red :: GL.Color4 GL.GLfloat
-        red = GL.Color4 0.8 0.1 0 1
-
-mkGear2 :: IO GL.DisplayList
-mkGear2 =
-    GL.defineNewList GL.Compile $ do
-        GL.colorMaterial GL.$= Just (GL.Front, GL.AmbientAndDiffuse)
-        GL.color green
-        makeGear 0.5 2 2 10 0.7
-      where
-        green :: GL.Color4 GL.GLfloat
-        green = GL.Color4 0 0.8 0.2 1
-
-mkGear3 :: IO GL.DisplayList
-mkGear3 =
-    GL.defineNewList GL.Compile $ do
-        GL.colorMaterial GL.$= Just (GL.Front, GL.AmbientAndDiffuse)
-        GL.color blue
-        makeGear 1.3 2 0.5 10 0.7
-      where
-        blue :: GL.Color4 GL.GLfloat
-        blue = GL.Color4 0.2 0.2 1 1
-
-makeGear :: Float -> Float -> Float -> Int -> Float -> IO ()
-makeGear inradius outradius width teeth toothdepth = do
-    let r0 = inradius
-        r1 = outradius - toothdepth / 2
-        r2 = outradius + toothdepth / 2
-        da = 2 * pi / realToFrac teeth / 4 :: Float
-    GL.shadeModel GL.$= GL.Flat
-    GL.normal (GL.Normal3 0 0 1 :: GL.Normal3 GL.GLfloat)
-
-    -- front face
-    GL.renderPrimitive GL.QuadStrip $
-        forM_ [0 .. teeth] $ \i -> do
-            let a = realToFrac i * 2 * pi / realToFrac teeth :: Float
-            GL.vertex $ mkVertex (r0 * cos a) (r0 * sin a) (width * 0.5)
-            GL.vertex $ mkVertex (r1 * cos a) (r1 * sin a) (width * 0.5)
-            when (i < teeth) $ do
-                GL.vertex $ mkVertex (r0 * cos a)            (r0 * sin a)            (width * 0.5)
-                GL.vertex $ mkVertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) (width * 0.5)
-
-    -- front sides of teeth
-    GL.renderPrimitive GL.Quads $
-        forM_ [0 .. pred teeth] $ \i -> do
-            let a = realToFrac i * 2 * pi / realToFrac teeth :: Float
-            GL.vertex $ mkVertex (r1 * cos a) (r1 * sin a) (width * 0.5)
-            GL.vertex $ mkVertex (r2 * cos (a + da)) (r2 * sin (a + da)) (width * 0.5)
-            GL.vertex $ mkVertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) (width * 0.5)
-            GL.vertex $ mkVertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) (width * 0.5)
-
-    GL.normal (GL.Normal3 0 0 (-1) :: GL.Normal3 GL.GLfloat)
-
-    -- back face
-    GL.renderPrimitive GL.QuadStrip $
-        forM_ [0 .. teeth] $ \i -> do
-            let a = realToFrac i * 2 * pi / realToFrac teeth :: Float
-            GL.vertex $ mkVertex (r1 * cos a) (r1 * sin a) (negate width * 0.5)
-            GL.vertex $ mkVertex (r0 * cos a) (r0 * sin a) (negate width * 0.5)
-            when (i < teeth) $ do
-                GL.vertex $ mkVertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) (negate width * 0.5)
-                GL.vertex $ mkVertex (r0 * cos a) (r0 * sin a) (negate width * 0.5)
-
-    -- back sides of teeth
-    GL.renderPrimitive GL.Quads $
-        forM_ [0 .. pred teeth] $ \i -> do
-            let a = realToFrac i * 2 * pi / realToFrac teeth :: Float
-            GL.vertex $ mkVertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) (negate width * 0.5)
-            GL.vertex $ mkVertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) (negate width * 0.5)
-            GL.vertex $ mkVertex (r2 * cos (a + da)) (r2 * sin (a + da)) (negate width * 0.5)
-            GL.vertex $ mkVertex (r1 * cos a) (r1 * sin a) (negate width * 0.5)
-
-    -- outward faces of teeth
-    GL.renderPrimitive GL.QuadStrip $ do
-        forM_ [0 .. pred teeth] $ \i -> do
-            let a = realToFrac i * 2 * pi / realToFrac teeth :: Float
-            GL.vertex $ mkVertex (r1 * cos a) (r1 * sin a) (width * 0.5)
-            GL.vertex $ mkVertex (r1 * cos a) (r1 * sin a) (negate width * 0.5)
-            let u = r2 * cos (a + da) - r1 * cos a
-                v = r2 * sin (a + da) - r1 * sin a
-                len = sqrt (u * u + v * v)
-                un = u / len
-                vn = v / len
-            GL.normal $ mkNormal vn (-un) 0
-            GL.vertex $ mkVertex (r2 * cos (a + da)) (r2 * sin (a + da)) (width * 0.5)
-            GL.vertex $ mkVertex (r2 * cos (a + da)) (r2 * sin (a + da)) (negate width * 0.5)
-            GL.normal $ mkNormal (cos a) (sin a) 0
-            GL.vertex $ mkVertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) (width * 0.5)
-            GL.vertex $ mkVertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) (negate width * 0.5)
-            let u' = r1 * cos (a + 3 * da) - r2 * cos (a + 2 * da)
-                v' = r1 * sin (a + 3 * da) - r2 * sin (a + 2 * da)
-            GL.normal $ mkNormal v' (-u') 0
-            GL.vertex $ mkVertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) (width * 0.5)
-            GL.vertex $ mkVertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) (negate width * 0.5)
-            GL.normal $ mkNormal (cos a) (sin a) 0
-
-        GL.vertex $ mkVertex (r1 * cos 0) (r1 * sin 0) (width * 0.5)
-        GL.vertex $ mkVertex (r1 * cos 0) (r1 * sin 0) (negate width * 0.5)
-
-    GL.shadeModel GL.$= GL.Smooth
-
-    -- inside radius cylinder
-    GL.renderPrimitive GL.QuadStrip $
-        forM_ [0 .. teeth] $ \i -> do
-            let a = realToFrac i * 2 * pi / realToFrac teeth :: Float
-            GL.normal $ mkNormal (negate $ cos a) (negate $ sin a) 0
-            GL.vertex $ mkVertex (r0 * cos a) (r0 * sin a) (negate width * 0.5)
-            GL.vertex $ mkVertex (r0 * cos a) (r0 * sin a) (width * 0.5)
-
-mkVertex :: Float -> Float -> Float -> GL.Vertex3 GL.GLfloat
-mkVertex x y z =
-    GL.Vertex3 (realToFrac x) (realToFrac y) (realToFrac z)
-
-mkNormal :: Float -> Float -> Float -> GL.Normal3 GL.GLfloat
-mkNormal x y z =
-    GL.Normal3 (realToFrac x) (realToFrac y) (realToFrac z)
 
 getCursorKeyDirections :: GLFW.Window -> IO (Float, Float)
 getCursorKeyDirections w = do
